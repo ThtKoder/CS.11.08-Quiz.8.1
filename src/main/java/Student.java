a2baa8f87e4be9d99eef31156876db42ecfc9948
ThtKoder <sanderd2026@student.cis.edu.hk>
2024-03-06 08:11:53 +0800

    done.

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -13,7 +13,7 @@ public class Student {
     }
 
     public boolean addSubject(String subjectName) {
-        for (Subject subject: subjects) {
+        for (Subject subject : subjects) {
             if (subject.getName().equals(subjectName)) return false;
         }
         subjects.add(new Subject(subjectName));
@@ -49,40 +49,53 @@ public class Student {
      * The method bestSubject will return the subject that the student is strongest in (the subject with the
      * current highest grade). If there are multiple subjects with an equal current high score, the first subject
      * with that score will be returned.
+     *
      * @return the name of the subject with the current highest score (a String).
      */
     public String bestSubject() {
+        String bestGrade = "";
+        int numberBest = 0;
 
-        return null;
-
+        for (int i = 0; i < subjects.size(); i++) {
+            if (subjects.get(i).getCurrentGrade() > numberBest) {
+                bestGrade = String.valueOf(subjects.get(i).getName());
+                numberBest = subjects.get(i).getCurrentGrade();
+            }
+        }
+        return bestGrade;
     }
 
     /**
      * The method averageGrade will return the average grade across all subjects that the student is taking (a double).
+     *
      * @return the students' average grade (a double).
      */
     public double averageGrade() {
+        float avgGrade = 0;
 
-        return 0.0;
+        for (int i = 0; i < subjects.size(); i++) {
+            avgGrade += subjects.get(i).getCurrentGrade();
+        }
 
+        avgGrade = avgGrade / subjects.size();
+
+        return avgGrade;
     }
 
     /**
      * The method reportCard will return a report card in the form of a String.
      * The report card will be in the following format:
-     *
+     * <p>
      * Report card for [name]
      * [Subject 1] [tab] [grade]
      * [Subject 2] [tab] [grade]
      * [Subject 3] [tab] [grade]
      * [Subject 4] [tab] [grade]
      * ...
+     *
      * @return the student's report card (a String).
      */
     public String reportCard() {
-
-        return null;
-
+        return "toString";
     }
-
 }
